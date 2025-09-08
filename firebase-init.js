ea9d73023a7a562d9e3dde9e8b40cabcc043645a
iamjake333-cmyk <iamjake333@aol.com>
2025-09-08 12:03:22 -0700

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBFvr693Fhe1rZrSD29VLVDwp1V9frHwQ",
+  authDomain: "gradeghost.firebaseapp.com",
+  projectId: "gradeghost",
+  storageBucket: "gradeghost.appspot.com",
+  messagingSenderId: "1064541107875",
+  appId: "1:1064541107875:web:9b6ebde0a4b3278e697116",
+  measurementId: "G-YTY7XK907Y"
+};
+
+firebase.initializeApp(firebaseConfig);
